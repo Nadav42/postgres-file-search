8495db0ce1939ff41de24ce56720b97854efa010
Nadav42 <bscpkd@Gmail.com>
2021-08-18 20:40:29 +0300

    scan

--- src/scan-files.ts
+++ src/scan-files.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { fileRecordDBService } from './file-record-db-service';
 
 //joining path of directory 
-const directoryPath = "C:/Users/Nadav/Downloads";
+const directoryPath = "C:/";
 console.log(directoryPath);
 
 class FolderScanner {
@@ -10,7 +10,12 @@ class FolderScanner {
 
 	}
 
-	async scanDirectoryRecursive(directoryPath: string) {
+	async scanDirectoryRecursive(directoryPath: string, depth: number = 0, maxDepth: number = 250) {
+		if (depth > maxDepth) {
+			console.log("reached depth limit...");
+			return;
+		}
+
 		fs.readdir(directoryPath, async (err, files) => {
 			//handling error
 			if (err) {
@@ -19,27 +24,27 @@ class FolderScanner {
 			}
 
 			// iterate all files
-			await this.processFiles(directoryPath, files);
+			await this.processFiles(directoryPath, files, depth, maxDepth);
 		});
 	}
 
-	async processFiles(directoryPath: string, files: string[]) {
+	async processFiles(directoryPath: string, files: string[], depth: number, maxDepth: number) {
 		for (let index = 0; index < files.length; index++) {
 			const file = files[index];
-			await this.processFile(directoryPath, file);
+			await this.processFile(directoryPath, file, depth, maxDepth);
 		}
 	}
 
-	async processFile(directoryPath: string, file: string) {
+	async processFile(directoryPath: string, file: string, depth: number, maxDepth: number) {
 		const filePath = `${directoryPath}/${file}`;
 		const stats = fs.statSync(filePath);
 
 		if (stats.isFile()) {
 			console.log(filePath, stats.birthtime, stats.mtime, stats.size);
 			await fileRecordDBService.insertFileRecord(filePath, stats.birthtime, stats.mtime, stats.size); // stats.ctime is changed time, created time is birthtime
-		} else if (stats.isDirectory()) {
+		} else if (stats.isDirectory() && !stats.isSymbolicLink()) {
 			console.log("found directory:", filePath);
-			await this.scanDirectoryRecursive(filePath);
+			await this.scanDirectoryRecursive(filePath, depth + 1, maxDepth);
 		}
 	}
 }
