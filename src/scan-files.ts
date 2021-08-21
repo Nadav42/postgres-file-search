f89e14bc9e5691a076565b1570f38ffb7ec548f1
Nadav42 <bscpkd@Gmail.com>
2021-08-22 00:23:27 +0300

    added support for onFinish and onStart callbacks (a way to know when the scan finished)

--- src/scan-files.ts
+++ src/scan-files.ts
@@ -2,21 +2,32 @@ import fs from 'fs';
 import { fileRecordDBService } from './file-record-db-service';
 
 //joining path of directory 
-const directoryPath = "C:/";
+const directoryPath = "C:/Program Files (x86)/Steam";
 const MIN_SIZE_IN_BYTES = 50 * 1024; // at least X kb
 
 class FolderScanner {
 	scannedFiles: number = 0;
 	scannedFolders: number = 0;
+	pathsProcessing: Map<string, boolean> = new Map<string, boolean>();
+	onStartCB: () => void;
+	onFinishCB: () => void;
 
-	constructor() {
-
+	constructor(onStartCB: () => void, onFinishCB: () => void) {
+		this.onStartCB = onStartCB;
+		this.onFinishCB = onFinishCB;
 	}
 
-	scanDirectoryRecursive(directoryPath: string, quick: boolean, depth: number = 0, maxDepth: number = 250) {
+	scanDirectory(directoryPath: string, quick: boolean) {
+		this.onStart();
+		this.scanDirectoryRecursive(directoryPath, quick);
+	}
+
+	private scanDirectoryRecursive(directoryPath: string, quick: boolean, depth: number = 0, maxDepth: number = 250) {
+		this.markProcessingPath(directoryPath);
 
 		if (depth > maxDepth) {
 			console.log("reached depth limit...");
+			this.unmarkProcessingPath(directoryPath);
 			return;
 		}
 
@@ -30,23 +41,25 @@ class FolderScanner {
 			} else {
 				console.log('Unable to scan directory: ' + err, directoryPath);
 			}
+			this.unmarkProcessingPath(directoryPath);
 		});
 	}
 
 	processFiles(directoryPath: string, files: string[], quick: boolean, depth: number, maxDepth: number) {
 		for (let index = 0; index < files.length; index++) {
 			const file = files[index];
-			quick ? this.processFileQuick(directoryPath, file, depth, maxDepth) : this.processFileFull(directoryPath, file, depth, maxDepth);
+			const filePath = `${directoryPath}/${file}`;
+			this.markProcessingPath(filePath);
+			quick ? this.processFileQuick(filePath, depth, maxDepth) : this.processFileFull(filePath, depth, maxDepth);
 		}
 	}
 
 	// quick scan because it will treat all paths with extensions as files and filter non allowed extensions
 	// this will however miss directories like Intellij 2021.3.4
-	processFileQuick(directoryPath: string, file: string, depth: number, maxDepth: number) {
-		const filePath = `${directoryPath}/${file}`;
-
+	processFileQuick(filePath: string, depth: number, maxDepth: number) {
 		if (!this.shouldScanFileQuick(filePath)) {
 			this.scannedFiles = this.scannedFiles + 1;
+			this.unmarkProcessingPath(filePath);
 			return;
 		}
 
@@ -59,20 +72,20 @@ class FolderScanner {
 						// console.log(filePath, stats.birthtime, stats.mtime, stats.size);
 						await fileRecordDBService.insertFileRecord(filePath, stats.birthtime, stats.mtime, stats.size); // stats.ctime is changed time, created time is birthtime
 					}
+					this.unmarkProcessingPath(filePath); // only unmark if it's a file, if it's a directory then the scanDirectory func will unmark it
 				} else if (stats.isDirectory() && !stats.isSymbolicLink()) {
 					this.scannedFolders = this.scannedFolders + 1;
 					this.scanDirectoryRecursive(filePath, true, depth + 1, maxDepth);
 				}
 			} else {
 				console.log(err);
+				this.unmarkProcessingPath(filePath);
 			}
 		});
 	}
 
 	// full scan because it will check if a path is a file or directory even when it doesn't have an extension
-	processFileFull(directoryPath: string, file: string, depth: number, maxDepth: number) {
-		const filePath = `${directoryPath}/${file}`;
-
+	processFileFull(filePath: string, depth: number, maxDepth: number) {
 		fs.stat(filePath, async (err, stats) => {
 			if (!err) {
 				if (stats.isFile()) {
@@ -81,12 +94,14 @@ class FolderScanner {
 						// console.log(filePath, stats.birthtime, stats.mtime, stats.size);
 						await fileRecordDBService.insertFileRecord(filePath, stats.birthtime, stats.mtime, stats.size); // stats.ctime is changed time, created time is birthtime
 					}
+					this.unmarkProcessingPath(filePath); // only unmark if it's a file, if it's a directory then the scanDirectory func will unmark it
 				} else if (stats.isDirectory() && !stats.isSymbolicLink()) {
 					this.scannedFolders = this.scannedFolders + 1;
 					this.scanDirectoryRecursive(filePath, false, depth + 1, maxDepth);
 				}
 			} else {
 				console.log(err);
+				this.unmarkProcessingPath(filePath);
 			}
 		});
 	}
@@ -105,6 +120,28 @@ class FolderScanner {
 		const extension = filePath.split(".").reverse()[0].toLowerCase();
 		return ["exe", "zip", "msi", "rar", "tar", "tar.gz", "jar"].includes(extension);
 	}
+
+	markProcessingPath(filePath: string) {
+		this.pathsProcessing.set(filePath, true);
+		// console.log("[markProcessingPath] this.pathsProcessing:", this.pathsProcessing.size);
+	}
+
+	unmarkProcessingPath(filePath: string) {
+		this.pathsProcessing.delete(filePath);
+		// console.log("[unmarkProcessingPath] this.pathsProcessing:", this.pathsProcessing.size);
+
+		if (this.pathsProcessing.size <= 0) {
+			this.onFinish();
+		}
+	}
+
+	onStart() {
+		this.onStartCB && this.onStartCB();
+	}
+
+	onFinish() {
+		this.onFinishCB && this.onFinishCB();
+	}
 }
 
 const runProgram = async () => {
@@ -112,21 +149,27 @@ const runProgram = async () => {
 
 	console.log(directoryPath);
 
-	const start = Date.now();
+	let start: number = 0;
 
-	const scanner = new FolderScanner();
-	
-	// TODO: alternate between quick scans and full scans
-	scanner.scanDirectoryRecursive(directoryPath, true);
-	// scanner.scanDirectoryRecursive(directoryPath, false);
+	const onStart = () => {
+		start = Date.now();
+	}
 
-	process.on('exit', () => {
+	const onFinish = () => {
 		const end = Date.now();
 		const timeTook = (end - start);
 		console.log(`finished ${directoryPath} in ${timeTook.toFixed(2)}ms`);
 		console.log(`scanned: files: ${scanner.scannedFiles} folders: ${scanner.scannedFolders}`);
 		console.log("exited");
-	});
+
+		// do whatever you want here, can save to DB how much time the scan took etc..
+	}
+
+	const scanner = new FolderScanner(onStart, onFinish);
+
+	// TODO: alternate between quick scans and full scans?
+	scanner.scanDirectory(directoryPath, true);
+	// scanner.scanDirectory(directoryPath, false);
 }
 
 
